8cfdc350bc7017c8c9ebf93c17cede74021e96a7
Luka Kotur <looko95@gmail.com>
2017-08-10 13:01:57 +0200

    Updated info

--- js/my.js
+++ js/my.js
@@ -1,28 +1,122 @@
 let ctx = document.getElementById('intakeChart').getContext('2d');
 
 let myChart = new Chart(ctx, {
-    type: 'line',
+    type: 'bar',
     data: {
         labels: ["Ponedeljak", "Utorak", "Sreda", "Cetvrtak", "Petak", "Subota", "Nedelja"],
-        datasets: [{
-            label: 'Unos kalorija',
-            data: [2130, 2444, 1800, 1950, 2000, 2100, 2440],
-            backgroundColor: [
-                'transparent'
-            ],
-            borderColor: [
-                '#3498DB',
-            ],
-            borderWidth: 1
-        },
+        datasets: [
             {
-                label: 'Optimalan unos kalorija',
-                data: [2400, 2400, 2400, 2400, 2400, 2400, 2400],
+                label: 'Voce',
+                data: [150, 200, 150, 150, 200, 100, 150],
                 backgroundColor: [
-                    'transparent'
+                    '#28B463',
+                    '#28B463',
+                    '#28B463',
+                    '#28B463',
+                    '#28B463',
+                    '#28B463',
+                    '#28B463'
                 ],
                 borderColor: [
                     '#28B463',
+                    '#28B463',
+                    '#28B463',
+                    '#28B463',
+                    '#28B463',
+                    '#28B463',
+                    '#28B463'
+                ],
+                borderWidth: 1,
+            },
+            {
+                label: 'Povrce',
+                data: [200, 150, 100, 200, 150, 200, 100],
+                backgroundColor: [
+                    '#FFC300',
+                    '#FFC300',
+                    '#FFC300',
+                    '#FFC300',
+                    '#FFC300',
+                    '#FFC300',
+                    '#FFC300'
+                ],
+                borderColor: [
+                    '#FFC300',
+                    '#FFC300',
+                    '#FFC300',
+                    '#FFC300',
+                    '#FFC300',
+                    '#FFC300',
+                    '#FFC300'
+                ],
+                borderWidth: 1,
+            },
+            {
+                label: 'Meso i mesne preradjevine',
+                data: [150, 300, 200, 300, 150, 300, 250],
+                backgroundColor: [
+                    '#FF5733',
+                    '#FF5733',
+                    '#FF5733',
+                    '#FF5733',
+                    '#FF5733',
+                    '#FF5733',
+                    '#FF5733'
+                ],
+                borderColor: [
+                    '#FF5733',
+                    '#FF5733',
+                    '#FF5733',
+                    '#FF5733',
+                    '#FF5733',
+                    '#FF5733',
+                    '#FF5733'
+                ],
+                borderWidth: 1,
+            },
+            {
+                label: 'Mlecni proizvodi',
+                data: [100, 150, 200, 100, 50, 100, 80],
+                backgroundColor: [
+                    '#273746',
+                    '#273746',
+                    '#273746',
+                    '#273746',
+                    '#273746',
+                    '#273746',
+                    '#273746'
+                ],
+                borderColor: [
+                    '#273746',
+                    '#273746',
+                    '#273746',
+                    '#273746',
+                    '#273746',
+                    '#273746',
+                    '#273746'
+                ],
+                borderWidth: 1,
+            },
+            {
+                label: 'Zitarice',
+                data: [80, 50, 90, 45, 50, 100, 80],
+                backgroundColor: [
+                    '#2962ff',
+                    '#2962ff',
+                    '#2962ff',
+                    '#2962ff',
+                    '#2962ff',
+                    '#2962ff',
+                    '#2962ff'
+                ],
+                borderColor: [
+                    '#2962ff',
+                    '#2962ff',
+                    '#2962ff',
+                    '#2962ff',
+                    '#2962ff',
+                    '#2962ff',
+                    '#2962ff'
                 ],
                 borderWidth: 1,
             }
@@ -35,13 +129,26 @@ let myChart = new Chart(ctx, {
                 ticks: {
                     beginAtZero: true,
                     min: 0,
-                    max: 3500
+                    max: 500
                 }
             }]
         },
+        tooltips: {
+            mode: 'index'
+        },
         legend: {
             display: true,
-            position: 'bottom'
+            position: 'bottom',
+            onHover: function(e) {
+                e.target.style.cursor = 'pointer';
+            }
+        },
+        hover: {
+            onHover: function(e) {
+                var point = this.getElementAtEvent(e);
+                if (point.length) e.target.style.cursor = 'pointer';
+                else e.target.style.cursor = 'default';
+            }
         }
     }
 });
@@ -51,24 +158,20 @@ let ctx2 = document.getElementById('consumedChart').getContext('2d');
 let myChart2 = new Chart(ctx2, {
     type: 'pie',
     data: {
-        labels: ["Ugljeni hidrati", "Proteini", "Belancevine", "Voce", "Povrce", "Pice i napici"],
+        labels: ["Ugljeni hidrati", "Proteini", "Masti"],
         datasets: [{
-            data: [40, 15, 15, 10, 5, 15],
+            data: [60, 30, 10],
             backgroundColor: [
                 '#3498DB',
                 '#FFC300',
                 '#DAF7A6',
-                '#FF5733',
-                '#28B463',
-                '#273746'
+                '#FF5733'
             ],
             borderColor: [
                 '#3498DB',
                 '#FFC300',
                 '#DAF7A6',
-                '#FF5733',
-                '#28B463',
-                '#273746'
+                '#FF5733'
             ],
             borderWidth: 1
         }]
@@ -77,7 +180,17 @@ let myChart2 = new Chart(ctx2, {
     options: {
         legend: {
             display: true,
-            position: 'bottom'
+            position: 'bottom',
+            onHover: function(e) {
+                e.target.style.cursor = 'pointer';
+            }
+        },
+        hover:{
+            onHover: function(e) {
+                var point = this.getElementAtEvent(e);
+                if (point.length) e.target.style.cursor = 'pointer';
+                else e.target.style.cursor = 'default';
+            }
         },
         tooltips: {
             callbacks: {
@@ -92,7 +205,8 @@ let myChart2 = new Chart(ctx2, {
                     var tooltipPercentage = tooltipData;
                     return tooltipLabel + ': ' + tooltipPercentage + '%';
                 }
-            }
+            },
+
         }
 
     }
